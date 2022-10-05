5a1d5860ea7a6c82b7ee83dbab49f5940167456f
tanjinaesha <eshaislam15@gmail.com>
2022-10-05 19:42:57 +0600

    jsx

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Hi from'./compo/Hello';
+import Car from'./compo/Class';
+
+function App() {
+  return (
+    <div className="App">
+      <Hi/><Car/>
+    </div>
+    
+    
+  );
+  
+}
+
+export default App;
---
+++ src/compo/Hello.js
@@ -0,0 +1,10 @@
+function index(){
+    return <h1>hi</h1>;
+}
+const a=(
+<h1>eshaaaa</h1>
+);
+function Hi(){
+    return <h1>{a}{index()}{100+90}</h1>;
+}
+export default Hi;
